119f192b46330f0fce532150bf932b495962dcac
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2023-10-25 14:03:19 -0300

    ajuste na strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -14,5 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	return (ft_memcmp(s1, s2, n));
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i < n && s1[i] && s2[i])
+	{
+		if ((unsigned char) s1[i] - (unsigned char) s2[i] != 0)
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char) s2[i]);
 }
