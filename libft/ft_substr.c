4b3679f2faf6b5ff1b58dd4ec297492e607718b2
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2023-11-01 09:27:08 -0300

    pequeno ajuste de sintax no substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: madias-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 11:05:00 by madias-m          #+#    #+#             */
-/*   Updated: 2023/10/26 12:08:04 by madias-m         ###   ########.fr       */
+/*   Updated: 2023/10/31 09:56:47 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*substr;
 
-	substr = (char *) ft_calloc(1, len + 1);
+	substr = (char *) ft_calloc(len + 1, 1);
 	if (start >= len)
 		return (substr);
 	if (!substr)
