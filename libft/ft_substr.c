c380109f48346314dc5b99dad8f41058014aee98
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2023-11-03 11:32:20 -0300

    correcoes de memory leak na substr. len substituido pelo resultado de strlen(s) quando este for menor. memmove substituido pelo strlcpy.

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: madias-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 11:05:00 by madias-m          #+#    #+#             */
-/*   Updated: 2023/10/31 09:56:47 by madias-m         ###   ########.fr       */
+/*   Updated: 2023/11/03 11:28:20 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,15 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*substr;
 
-	substr = (char *) ft_calloc(len + 1, 1);
-	if (start >= len)
-		return (substr);
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	substr = ft_calloc(len + 1, 1);
 	if (!substr)
 		return (0);
-	return ((char *) ft_memmove(substr, &s[start], len));
+	ft_strlcpy(substr, &s[start], len + 1);
+	return (substr);
 }
