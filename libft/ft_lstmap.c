48a03a5fea9e8c59484ba768335f3258b3c0b904
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2023-11-03 11:13:49 -0300

    bonus finalizado

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,20 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/01 19:13:54 by madias-m          #+#    #+#             */
-/*   Updated: 2023/11/01 19:51:45 by madias-m         ###   ########.fr       */
+/*   Created: 2023/11/01 19:20:57 by madias-m          #+#    #+#             */
+/*   Updated: 2023/11/03 10:48:48 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	if (lst -> next)
-		ft_lstiter(lst -> next, f);
-	f(lst -> content);
+	t_list	*first_node;
+	t_list	*next;
+
+	first_node = 0;
+	while (lst)
+	{
+		next = ft_lstnew(f(lst -> content));
+		if (!next)
+		{
+			ft_lstclear(&first_node, del);
+			return (0);
+		}
+		ft_lstadd_back(&first_node, next);
+		lst = lst -> next;
+	}
+	return (first_node);
 }
