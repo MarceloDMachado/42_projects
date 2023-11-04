fffbf70c5aa42639dd5676a812caeec84dbb099a
Marcelo Dias Machado <madias-m@c2r7p4.42sp.org.br>
2023-11-04 13:29:02 -0300

    include da libft removido do header. Adicionado as libs necessarias para o projeto

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:10:42 by madias-m          #+#    #+#             */
-/*   Updated: 2023/11/04 13:15:36 by madias-m         ###   ########.fr       */
+/*   Updated: 2023/11/04 13:28:24 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "./libft/libft.h"
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
