98f87ad0b2f0ef9665cee1bc7cec6755d932b532
Marcelo Dias Machado <madias-m@c2r3p1.42sp.org.br>
2023-11-21 18:51:20 -0300

    removido o Make, este não é necessário.

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,22 +6,31 @@
 /*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:17:05 by madias-m          #+#    #+#             */
-/*   Updated: 2023/11/16 20:28:10 by madias-m         ###   ########.fr       */
+/*   Updated: 2023/11/21 18:38:27 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*read_file(int fd, char *line)
+{
+	char *buff;
+
+	buff = malloc(BUFFER_SIZE * sizeof(char));
+	if (!buff)
+		return (0);
+	fd++;
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*c = 0;
-
-	while(read(fd, c, 1) > 0)
-	{
-		write(1, c, 1);
-	}
+	char	*line;
 	
-	write(1, "\n", 1);
-
-	return (NULL);
+	if (BUFFER_SIZE <= 0)
+		return (0);
+	write(1, "teste", 5);
+	line = 0; 
+	line = read_file(fd, line);
+	return (line);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,36 +6,19 @@
 /*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:49:46 by madias-m          #+#    #+#             */
-/*   Updated: 2023/11/16 20:59:48 by madias-m         ###   ########.fr       */
+/*   Updated: 2023/11/21 18:19:38 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "get_next_line.h"
 
-
-static char    *test(void)
- {
-     char *c;
-     int fd;
-
-     fd = open("teste.txt", O_RDONLY);
-     while(read(fd, c, 1) > 0)
-     {
-         write(1, c, 1);
-     }
-
-     write(1, "\n", 1);
-     close(fd);
-     return (0);
- }
-
 int	main(void)
 {
 	int	fd;
 
-	//fd = open("teste.txt", O_RDONLY);
-	test();
-	//close(fd);
+	fd = open("teste.txt", O_RDONLY);
+	get_next_line(fd);
+	close(fd);
 	return (0);
 }
