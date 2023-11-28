8da322d883bb6d7751cac43a09bf5f9f525f399d
Marcelo Dias Machado <madias-m@c1r8p1.42sp.org.br>
2023-11-28 19:29:14 -0300

    criado a funcão ft_base_converter para transformacao de um numero inteiro em uma string sua base convertida ex.:(int 15 -> F)

---
+++ ft_printf/libft/ft_base_converter.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_base_converter.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madias-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 17:07:01 by madias-m          #+#    #+#             */
+/*   Updated: 2023/11/28 19:26:53 by madias-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*build_return(char *str, int is_negative, int size)
+{
+	int		i;
+	char	*s;
+
+	s = ft_calloc(size + is_negative, sizeof(char));
+	if (!s)
+		return (NULL);
+	i = 0;
+	if (is_negative)
+		s[i++] = 45;
+	while (size >= is_negative)
+		s[i++] = *(str + --size);
+	return (s);
+}
+
+static char	*convert(long int nbr, int base, char *base_chars)
+{
+	char	digits[33];
+	int		index;
+	int		orig_nbr;
+
+	orig_nbr = nbr;
+	if (nbr < 0)
+		nbr *= -1;
+	index = 0;
+	while (nbr > 0)
+	{
+		digits[index] = base_chars[(nbr % base)];
+		if (base != 10)
+			nbr /= base;
+		else
+			nbr = (nbr - (nbr % 10)) / 10;
+		index++;
+	}
+	return (build_return(digits, orig_nbr < 0, index));
+}
+
+static int	check_base(char *base, int len)
+{
+	int	i;
+
+	if (len-- < 2)
+		return (0);
+	while (len >= 0)
+	{
+		if (base[len] == 43 || base[len] == 45)
+			return (0);
+		i = 0;
+		while (base[i])
+		{
+			if (base[i] == base[len] && i != len)
+				return (0);
+			i++;
+		}
+		len--;
+	}
+	return (1);
+}
+
+char	*ft_base_converter(int nbr, char *base)
+{	
+	long int	long_nb;
+	int			len;
+
+	long_nb = (long int) nbr;
+	len = 0;
+	while (base[len])
+		len++;
+	if (check_base(base, len))
+		return (convert(long_nb, len, base));
+	else
+		return (NULL);
+}
--- ft_printf/libft/libft.h
+++ ft_printf/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: madias-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 11:31:37 by madias-m          #+#    #+#             */
-/*   Updated: 2023/11/01 11:54:18 by madias-m         ###   ########.fr       */
+/*   Updated: 2023/11/28 18:47:52 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+char	*ft_base_converter(int nbr, char *base);
 #endif
