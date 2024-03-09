0bbcbe292a425643f4309256d22b5538340a964a
Marcelo Dias Machado <madias-m@c2r1p1.42sp.org.br>
2024-03-09 14:53:25 -0300

    desenvolviento do make file

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/09 13:54:32 by madias-m          #+#    #+#             */
-/*   Updated: 2024/03/09 14:05:08 by madias-m         ###   ########.fr       */
+/*   Created: 2024/03/09 13:56:04 by madias-m          #+#    #+#             */
+/*   Updated: 2024/03/09 14:00:45 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-int sim(void)
-{
-	ft_printf("uhul!");
-	return (1);
-}
+# include "libft/libft.h"
+
+int sim(void);
+#endif
+
