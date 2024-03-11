d73da4253bf180d92b0720a0b248be1fa6ae1eee
Marcelo Dias Machado <madias-m@c3r1p5.42sp.org.br>
2024-03-11 20:45:38 -0300

    .

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 13:56:04 by madias-m          #+#    #+#             */
-/*   Updated: 2024/03/11 18:56:08 by madias-m         ###   ########.fr       */
+/*   Updated: 2024/03/11 20:09:28 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define SO_LONG_H
 
 # include "../libft/libft.h"
+
+typedef struct s_game {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_leng;
+	int		endian;
+}	t_data;
+
 
 int sim(void);
 void	just_say_no(void);
