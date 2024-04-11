1a5c5feabf341bc1070143e7c810aa2b2e3d5d46
Marcelo Dias Machado <madias-m@c2r12p1.42sp.org.br>
2024-04-11 19:19:19 -0300

    separacao de escopos parent/child em funcoes

--- pipex/main.c
+++ pipex/main.c
@@ -6,21 +6,18 @@
 /*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 10:36:26 by madias-m          #+#    #+#             */
-/*   Updated: 2024/04/07 16:11:31 by madias-m         ###   ########.fr       */
+/*   Updated: 2024/04/07 19:47:54 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/wait.h>
+#include "includes/pipex.h"
 #include <stdlib.h>
 
 int	*get_pid_array(int argc)
 {
 	int	*pid_array;;
 
-	pid_array = calloc((argc - 2), sizeof(int));
+	pid_array = ft_calloc((argc - 2), sizeof(int));
 	return (pid_array);
 }
 
@@ -42,34 +39,41 @@ int *fork_all(int *pid_array, int argc)
 	return (pid_array);
 }
 
-int	func(void)
+void	parent_process(int **ids)
 {
-	static int	a;
-
-	return (a++);
+	int *init;
+	int	status;
+	
+	init = *ids;
+	while (**ids)
+	{
+		waitpid(**ids, &status, 0);
+		(*ids)++;
+	}
+	printf("\n");
+	free(init);
+	ids = 0;
 }
 
-int	main(int argc, char **argv)
+void	child_process(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+		printf("%d", i++);
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	int	*ids;
-	int	n;
-	int	i;
 
 	if (argc < 5)
-		return (printf("invalid arguments count"));
+		return (ft_printf("invalid arguments count"));
 	ids = fork_all(get_pid_array(argc), argc);
-	if (!ids)
-		n = 1;
-	else 
-		n = 6;
 	if (ids != 0)
-		wait(0);
-	i = 0;
-	printf("%d", func());
-	while (i < n + 5)
-		printf("%d", i++);
-	printf("\n");
-	if (ids != 0)
-		free(ids);
+		parent_process(&ids);
+	else
+		child_process(argc, argv);
 	return (0);
 }
