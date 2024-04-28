ff9d8292f8f2fb23c57d5e0ebe66701c42102e8f
Marcelo Dias Machado <madias-m@c1r6p1.42sp.org.br>
2024-04-28 18:51:58 -0300

    criado o metodo que busca o path no envp

--- test_pipex/src/exec_handler.c
+++ test_pipex/src/exec_handler.c
@@ -12,11 +12,27 @@
 
 #include "../includes/pipex.h"
 
+char	*find_path(t_ctrl *data, char *program)
+{
+	char	*path;
+	int		i;
+
+	i = 0;
+	path = 0;
+	while (data->envp[i] && !path)
+		path = ft_strnstr(data->envp[i++], "PATH", 5);
+	if (path)
+		path = ft_strchr(path, '=') + 1;
+	return (path);
+}
+
 void	ft_exec_cmd(t_ctrl *data, char *cmd)
 {
 	char	**splitted;
+	char	*path;
 
 	splitted = ft_split(cmd, " ");
+	path = find_path(data, splitted[0]);
 	execve(splitted[0], splitted, data->envp);
 	ft_free_matrix(splitted);
 }
