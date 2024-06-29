efa67fa0bb0655fa269a80c907fdad3bca803ebc
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2024-06-29 09:25:04 -0300

    norminette

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -36,6 +36,6 @@ void	rrb(void);
 void	rrr(void);
 void	print(int **(*stack)(void));
 void	swap_pointer(int **a, int **b);
-void    fix_stack(int **(*stack)(void));
+void	fix_stack(int **(*stack)(void));
 
 #endif
