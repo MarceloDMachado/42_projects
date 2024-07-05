ea60485359f703ff9e9a7f455e75abc3632e8f75
Marcelo Dias Machado <madias-m@c3r13p3.42sp.org.br>
2024-07-05 13:28:45 -0300

    ajuste na logica de ordernacao;

--- push_swap/src/sort.c
+++ push_swap/src/sort.c
@@ -15,12 +15,24 @@
 void	(*get_rotate_func(int **(*f)(void), int i)) (void)
 {
 	int		len;
+	void	(*func)(void);
 
 	len = stack_len(f);
 	if (i >= (len / 2))
-		return (rra);
+	{
+		if (f == get_stack_a)
+			func = rra;
+		else
+			func = rrb;
+	}
 	else
-		return (ra);
+	{
+		if (f == get_stack_a)
+			func = ra;
+		else
+			func = rb;
+	}
+	return (func);
 }
 
 int	get_index_of(int **(*stack)(void), int *element)
@@ -48,16 +60,58 @@ int	*get_min(int **(*stack)(void))
 	return (min);
 }
 
+int	*get_max(int **(*stack)(void))
+{
+	int	*max;
+	int	i;
+
+	i = 0;
+	max = stack()[i];
+	while (++i < stack_len(stack))
+	{
+		if (*(stack()[i]) > *max)
+			max = stack()[i];
+	}
+	return (max);
+}
+
+int	*get_smaller_than(int **(*stack)(void), int *n)
+{
+	int	*smaller;
+	int	i;
+
+	i = stack_len(stack) - 1;
+	smaller = 0;
+	while (i > 0)
+	{
+		if (*(stack()[i]) < *n && *(stack()[i - 1]) > *n)
+			smaller = stack()[i];
+		i--;
+	}
+	if (*(stack()[i]) < *n && *(stack()[stack_len(stack) - 1]) > *n)
+			smaller = stack()[i];
+	return (smaller);
+}
+
+
 void	sort(void)
 {
+	while (get_index_of(get_stack_a, get_min(get_stack_a)) != 0)
+		get_rotate_func(get_stack_a, get_index_of(get_stack_a, get_min(get_stack_a)))();
+	pb();
 	while (get_stack_a()[0])
 	{
-		while (get_index_of(get_stack_a, get_min(get_stack_a)) != 0)
-			get_rotate_func(get_stack_a, get_index_of(get_stack_a, get_min(get_stack_a)))();
-		pb();
+		if (*(get_stack_a()[0]) > *(get_max(get_stack_b)))
+			pb();
+		else
+		{
+			while (get_index_of(get_stack_b, get_smaller_than(get_stack_b, get_stack_a()[0])) != 0)
+				get_rotate_func(get_stack_b, get_index_of(get_stack_b, get_smaller_than(get_stack_b, get_stack_a()[0])))();
+			pb();
+		}
 	}
+	while (get_index_of(get_stack_b, get_max(get_stack_b)) != 0)
+		get_rotate_func(get_stack_b, get_index_of(get_stack_b, get_max(get_stack_b)))();
 	while (get_stack_b()[0])
-	{
 		pa();
-	}
 }
