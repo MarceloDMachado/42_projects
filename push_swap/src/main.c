78af688486169d1e16beb4b14f0e4a4f1c9ea70f
Marcelo Dias Machado <madias-m@c1r2p3.42sp.org.br>
2024-07-31 12:24:08 -0300

    utilizando o std error para display de error

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -85,7 +85,7 @@ int	main(int argc, char **argv)
 	if (argc <= 2)
 		return (0);
 	if (check_params(&argv[1]) || check_dups(&argv[1]))
-		return (write(1, "Error\n", 6) / 6);
+		return (write(2, "Error\n", 6) / 6);
 	fill_stack(stack_a, &argv[1]);
 	handle_sort()();
 	free_func_matrix();
