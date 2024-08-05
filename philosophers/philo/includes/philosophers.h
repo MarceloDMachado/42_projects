62421110c26b4a8b1c81d446cb4b72afb8beda95
Marcelo Dias Machado <madias-m@c1r7p3.42sp.org.br>
2024-08-05 17:37:30 -0300

    iniciando o philosophers

---
+++ philosophers/philo/includes/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madias-m <madias-m@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 17:13:17 by madias-m          #+#    #+#             */
+/*   Updated: 2024/08/05 17:36:43 by madias-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#endif
