db408155671159f21621c4fe90128f82c8386ffa
Marcelo Dias Machado <madias-m@c1r3p4.42sp.org.br>
2024-08-14 19:17:00 -0300

    iniciando o projeto

--- philosophers/philo/includes/philosophers.h
+++ philosophers/philo/includes/philosophers.h
@@ -13,8 +13,20 @@
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+
+typedef struct s_data
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+}	t_data;
+
+int	ft_atoi(const char *nptr);
 
 #endif
--- philosophers/philo/src/main.c
+++ philosophers/philo/src/main.c
@@ -12,8 +12,23 @@
 
 #include "../includes/philosophers.h"
 
-int main(void)
+void	init_data(t_data *data, int argc, char **argv)
 {
-	free(malloc(16));
-	return (1);
+	data->number_of_philosophers = ft_atoi(argv[0]);
+	data->time_to_die = ft_atoi(argv[1]);
+	data->time_to_eat = ft_atoi(argv[2]);
+	data->time_to_sleep = ft_atoi(argv[3]);
+	if (argc < 6)
+		return ;
+	data->number_of_times_each_philosopher_must_eat = ft_atoi(argv[4]);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (argc < 5 || argc > 6)
+		return (1);
+	init_data(&data, argc, &argv[1]);
+	return (0);
 }
