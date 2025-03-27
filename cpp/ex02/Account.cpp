0194a041b396af768e5e279228945ccecccf358c
Marcelo Dias Machado <madias-m@c1r4p1.42sp.org.br>
2025-03-27 15:02:43 -0300

    criado o .cpp

--- cpp/ex02/Account.cpp
+++ cpp/ex02/Account.cpp
@@ -6,9 +6,29 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 14:10:00 by madias-m          #+#    #+#             */
-/*   Updated: 2025/03/27 14:11:11 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/03/27 14:57:03 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Account.hpp"
+#include "./Account.hpp"
 
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+}
+
+static int	getNbAccounts( void ){
+	
+}
+static int	getTotalAmount( void ){
+	
+}
+static int	getNbDeposits( void ){
+	
+}
+static int	getNbWithdrawals( void ){
+	
+}
+static void	displayAccountsInfos( void ){
+	
+}
