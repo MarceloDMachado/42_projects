aa74c44d454773ca8aa5f6ca19a43c4ff8750e94
Marcelo Dias Machado <madias-m@c1r3p4.42sp.org.br>
2025-03-31 16:02:45 -0300

    cpp01ex01 concluido

---
+++ cpp01/ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/28 18:04:46 by madias-m          #+#    #+#             */
+/*   Updated: 2025/03/31 16:01:35 by madias-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string		*stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "str address: " << &str << std::endl;
+	std::cout << "address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "str value: " << str << std::endl;
+	std::cout << "value by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value by stringREF: " << stringREF << std::endl;
+	return (0);
+}
