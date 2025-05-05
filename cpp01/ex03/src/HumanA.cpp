c061da75766abca7b18520cf14446e6ed9775b31
Marcelo Dias Machado <madias-m@c1r6p2.42sp.org.br>
2025-05-05 14:56:15 -0300

    ajuste na fala do attack()

--- cpp01/ex03/src/HumanA.cpp
+++ cpp01/ex03/src/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:57:47 by madias-m          #+#    #+#             */
-/*   Updated: 2025/04/01 12:20:13 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/05 14:55:40 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ HumanA::HumanA(std::string name, Weapon weapon)
 
 void		HumanA::attack(void)
 {
-	std::cout << this->name << "attacks with their " << this->weapon.getType() << std::endl;
+	std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
 }
 
 
--- cpp01/ex03/src/HumanB.cpp
+++ cpp01/ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:58:02 by madias-m          #+#    #+#             */
-/*   Updated: 2025/04/01 13:05:48 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/05 14:55:50 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ HumanB::HumanB(std::string name)
 
 void	HumanB::attack(void)
 {
-	std::cout << this->name << "attacks with their " << this->weapon.getType() << std::endl;
+	std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon weapon)
