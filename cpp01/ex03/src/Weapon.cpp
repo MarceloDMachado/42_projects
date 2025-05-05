6674364e5701cf8298583f019e5391bbb56a230e
Marcelo Dias Machado <madias-m@c1r6p2.42sp.org.br>
2025-05-05 16:41:53 -0300

    removendo a alocacao para type no Weapon

--- cpp01/ex03/src/Weapon.cpp
+++ cpp01/ex03/src/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:14:50 by madias-m          #+#    #+#             */
-/*   Updated: 2025/05/05 15:27:21 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:41:14 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@ Weapon::Weapon(void){}
 
 Weapon::Weapon(std::string type)
 {
-	this->type = new std::string(type);
+	this->type = type;
 }
 
 const std::string	Weapon::getType(void)
 {
-	return (*this->type);
+	return (this->type);
 }
 
 void		Weapon::setType(std::string type)
 {
-	delete this->type;
-	this->type = new std::string(type);
+	this->type = type;
 }
