fd9e31ea03a0158af66f84c9b605bd6a9fb75bfb
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2025-05-05 21:37:05 -0300

    Alterando o weapon de HumanB para pointer e modificando o setWeapon para receber uma ref de weapon

--- cpp01/ex03/src/HumanB.cpp
+++ cpp01/ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:58:02 by madias-m          #+#    #+#             */
-/*   Updated: 2025/05/05 14:55:50 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:34:17 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ HumanB::HumanB(std::string name)
 
 void	HumanB::attack(void)
 {
-	std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
+	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 }
 
-void	HumanB::setWeapon(Weapon weapon)
+void	HumanB::setWeapon(Weapon& weapon)
 {
-	this->weapon = weapon;
+	this->weapon = &weapon;
 }
--- cpp01/ex03/src/HumanB.hpp
+++ cpp01/ex03/src/HumanB.hpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:57:58 by madias-m          #+#    #+#             */
-/*   Updated: 2025/04/01 13:03:33 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:34:00 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ class HumanB
 {
 	private:
 		std::string	name;
-		Weapon		weapon;
+		Weapon		*weapon;
 		HumanB(void);
 	public:
 		HumanB(std::string name);
 		void		attack(void);
-		void		setWeapon(Weapon weapon);
+		void		setWeapon(Weapon& weapon);
 };
 
 #endif
