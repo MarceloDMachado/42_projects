583d47a17daf0f0173d58b9b480441434668fdf7
Marcelo Dias Machado <madias-m@c1r2p1.42sp.org.br>
2025-05-07 14:01:17 -0300

    finalizando o ex06

--- cpp01/ex06/src/Harl.cpp
+++ cpp01/ex06/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:46:38 by madias-m          #+#    #+#             */
-/*   Updated: 2025/05/06 22:35:35 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/07 14:00:42 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,40 +32,59 @@ void    Harl::error(void)
     std::cout << "[ERROR] Example of error message!!!" << std::endl;
 }
 
+std::string to_lower(const char *str)
+{
+    std::string result;
+    
+    while (*str)
+        result.push_back(::tolower(*str++));
+    return (result);
+}
+
+int     get_index(std::string level)
+{
+    int i;
+    std::string methods[5] = {"debug", "info", "warning", "error"};
+
+    i = 0;
+    while (i < 4)
+    {
+        if (methods[i].compare(level) == 0)
+            return (i);
+        i++;
+    }
+    return (-1);
+}
+
 void    Harl::complain(std::string level)
 {
     int index;
-    std::map<std::string, void (Harl::*)(void)> myPrivateMethodsMap;
-    // myPrivateMethodsMap["debug"] = &Harl::debug;
-    // myPrivateMethodsMap["info"] = &Harl::info;
-    // myPrivateMethodsMap["warning"] = &Harl::warning;
-    // myPrivateMethodsMap["error"] = &Harl::error;
-
+    static std::map<std::string, void (Harl::*)(void)> myPrivateMethodsMap;
     myPrivateMethodsMap.insert(std::make_pair("debug", &Harl::debug));
     myPrivateMethodsMap.insert(std::make_pair("info", &Harl::info));
     myPrivateMethodsMap.insert(std::make_pair("warning", &Harl::warning));
     myPrivateMethodsMap.insert(std::make_pair("error", &Harl::error));
     
+    level = to_lower(level.c_str());
     if (myPrivateMethodsMap.find(level) == myPrivateMethodsMap.end())
     {
         std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
         return ;
     }
-    
-    index = std::distance(myPrivateMethodsMap.begin(), myPrivateMethodsMap.find(level));
+    index = get_index(level);
     switch (index)
     {
         case 0:
-            (this->*(myPrivateMethodsMap["debug"]))();
+            (this->*myPrivateMethodsMap["debug"])();
             index++;
         case 1:
-            (this->*(myPrivateMethodsMap["info"]))();
+            (this->*myPrivateMethodsMap["info"])();
             index++;
         case 2:
-            (this->*(myPrivateMethodsMap["warning"]))();
+            (this->*myPrivateMethodsMap["warning"])();
             index++;
         case 3:
-            (this->*(myPrivateMethodsMap["error"]))();
+            (this->*myPrivateMethodsMap["error"])();
             index++;
     }
 }
--- cpp01/ex06/src/main.cpp
+++ cpp01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 21:27:33 by madias-m          #+#    #+#             */
-/*   Updated: 2025/05/06 22:27:54 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/05/07 13:47:59 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ int main(int argc, char **argv)
 {
     Harl obj = Harl();
     
-    argc = argc + 1;
+    if (argc != 2)
+    {
+        std::cerr << "Invalid arguments!\n";
+        return (1);
+    }
     obj.complain(argv[1]);
     
     return (0);
