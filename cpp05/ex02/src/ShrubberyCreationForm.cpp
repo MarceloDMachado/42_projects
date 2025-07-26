f9f19c5a7b9ebf1eb4520bf850599461a01a866f
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2025-07-26 12:38:25 -0300

    Implementando a Shrubbery

--- cpp05/ex02/src/ShrubberyCreationForm.cpp
+++ cpp05/ex02/src/ShrubberyCreationForm.cpp
@@ -6,23 +6,24 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 13:25:48 by madias-m          #+#    #+#             */
-/*   Updated: 2025/06/17 15:05:39 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/07/25 22:11:07 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(void):NAME("default"),GRADE_TO_SIGN(25),GRADE_TO_EXECUTE(5)
+ShrubberyCreationForm::ShrubberyCreationForm(void):NAME("default"),GRADE_TO_SIGN(145),GRADE_TO_EXECUTE(137)
 {
     this->isSigned = false;
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm(std::string name):NAME(name),GRADE_TO_SIGN(25),GRADE_TO_EXECUTE(5)
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target):NAME("ShrubberyCreationForm"),GRADE_TO_SIGN(145),GRADE_TO_EXECUTE(137)
 {
+    this->target = target;
     this->isSigned = false;
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other):NAME(other.NAME),GRADE_TO_SIGN(25),GRADE_TO_EXECUTE(5)
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other):NAME(other.NAME),GRADE_TO_SIGN(145),GRADE_TO_EXECUTE(137)
 {
     *this = other;
 }
@@ -30,7 +31,10 @@ ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other)
 ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
 {
     if (this != &other)
+    {
+        this->target = other.target;    
         this->isSigned = other.isSigned;
+    }
     return (*this);
 }
 
@@ -39,5 +43,12 @@ ShrubberyCreationForm::~ShrubberyCreationForm(void){}
 void	ShrubberyCreationForm::execute(Bureaucrat const& executor) const
 {
 	std::cout << executor.getName() << " is trying to execute contract";
+}
+
+void    ShrubberyCreationForm::plantShrubbery(void) const 
+{
+    std::ofstream file(std::string(target).append("_shrubbery"));
+
+    file << " " << std::endl;
 }
 
