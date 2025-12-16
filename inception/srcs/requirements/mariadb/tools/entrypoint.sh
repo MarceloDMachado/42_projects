8676b3aa3821f333747c581ccd6944c0d5adb937
mrl.marcelodias@gmail.com <mrl.marcelodias@gmail.com>
2025-12-16 13:17:32 -0300

    preparando inception para entrega

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,33 +1,25 @@
-#!/bin/sh
-set -e
+#!/bin/ash
 
-DATA_DIR=$MARIADB_DATA_DIR
-RUN_DIR=$MARIADB_RUN_DIR
-LOG_DIR=$MARIADB_LOG_DIR
+envsubst '$MARIADB_PORT' < /etc/my.cnf.d/99_my.cnf > /etc/my.cnf.d/mariadb-server.cnf
 
-mkdir -p $DATA_DIR $RUN_DIR $LOG_DIR
-chown -R mysql:mysql $DATA_DIR $RUN_DIR $LOG_DIR
+mariadbd --user=mysql --skip-networking > /dev/null 2>&1 &
+pid=$!
 
-# Inicializa o banco se estiver vazio
-if [ ! -d "$DATA_DIR/mysql" ]; then
-    echo "Inicializando MariaDB..."
-    mariadb-install-db --user=mysql --datadir=$DATA_DIR > /dev/null 2>&1
-    mysqld_safe --datadir=$DATA_DIR &
-    
-    sleep 5
-    
-    if [ -f ./init.sql ]; then
-        echo "Executando ./init.sql..."
-        mariadb -u root < ./init.sql
-    fi
+until mariadb-admin ping --silent; do
+	sleep 1
+done
 
-    if [ -f ./insert_users.sql ]; then
-         echo "Executando ./insert_users.sql..."
-         mariadb -u root < ./insert_users.sql
-    fi
-    touch test.txt
-
-    mysqladmin -u root shutdown
+if [ $(mariadb -u root -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$MARIADB_USER');") -eq 0 ];
+then
+	mariadb -u root -sse "
+	CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;
+	CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$(cat /run/secrets/mariadb_root_password)';
+	GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+	CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$(cat /run/secrets/mariadb_user_password)';
+	GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;
+	FLUSH PRIVILEGES;"
 fi
 
-exec mysqld_safe --datadir=$DATA_DIR
+kill $pid
+
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
--- inception/srcs/requirements/mariadb/tools/insert_users.sql
+++
@@ -1,6 +0,0 @@
-USE app_db;
-
--- Adiciona usuários de exemplo
-INSERT INTO usuarios (usuario, senha) VALUES ('admin', '123456');
-INSERT INTO usuarios (usuario, senha) VALUES ('usuario1', 'senha1');
-INSERT INTO usuarios (usuario, senha) VALUES ('usuario2', 'senha2');
