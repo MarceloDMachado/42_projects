f08c2569a229bc9d853ea7df2286306eac865afa
Marcelo Dias Machado <madias-m@c1r10p1.42sp.org.br>
2026-01-07 09:42:18 -0300

    ajuste na main do ex01 para que valide o input dentro de um try_catch

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 10:42:21 by madias-m          #+#    #+#             */
-/*   Updated: 2026/01/07 09:40:10 by madias-m         ###   ########.fr       */
+/*   Updated: 2026/01/07 09:41:20 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	main(int argc, char** argv)
 {
-	if (argc < 2)
-		throw std::runtime_error("Error");
-
-	RPN	rpn;
-
 	try
 	{
+		if (argc != 2)
+			throw std::runtime_error("Error");
+
+		RPN	rpn;
 		std::cout << rpn.calcRPN(argv[1]) << std::endl;
 	}
 	catch(const std::exception& e)
